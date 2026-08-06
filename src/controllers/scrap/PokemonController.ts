acb4d605dce621d2db4329b629bed314c8693f9d
Alifdoll <alifianfajarpratama@gmail.com>
2026-08-06 16:55:03 +0700

    ffuck shit up

--- src/controllers/scrap/PokemonController.ts
+++ src/controllers/scrap/PokemonController.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import Controller from '../Controller';
 import { Request, Response, Router } from 'express';
+import { load } from 'cheerio';
 
 class PokemonController extends Controller {
   private router: Router;
@@ -21,8 +23,41 @@ class PokemonController extends Controller {
   public async index(req: Request, res: Response): Promise<Response> {
     try {
       const request_body = req.body;
+      console.log('🚀 ~ PokemonController ~ index ~ html_data:');
+
+      const url = 'https://www.octaveclothing.com/men';
+      const result: Record<string, string>[] = [];
+
+      const response = await axios.get(url);
+      // const html_data = response.data;
+      // // console.info("🚀 ~ PokemonController ~ index ~ const:", const)
+
+      const $ = load(html_data);
+
+      // const keys = ['Title', 'Description', 'Price'];
+      // const selectedElem = '.views-infinite-scroll-content-wrapper > .row > .col-6 > .product-7 > .product-body';
+
+      // $(selectedElem).each((parentIndex, parentElem) => {
+      //   let keyIndex = 0;
+      //   const data: Record<string, string> = {};
+
+      //   if (parentIndex) {
+      //     $(parentElem)
+      //       .children()
+      //       .each((_childId, childElem) => {
+      //         const value = $(childElem).text().trim();
+      //         if (value && keys[keyIndex]) {
+      //           data[keys[keyIndex]] = value;
+      //           keyIndex++;
+      //         }
+      //       });
+      //     result.push(data);
+      //   }
+      // });
+
       return super.success(res, 'success', {
         some_val: 'This is testing',
+        content: result,
       });
     } catch (error: any) {
       console.error(error.message);
